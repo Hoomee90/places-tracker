9800ca034941f7e30ea7aae94cda7c4ffe64568c
Samantha Callie <Hoomee90@gmail.com>
2024-04-19 13:51:55 -0700

    add routing for future views

--- PlacesTracker/Controllers/HomeController.cs
+++ PlacesTracker/Controllers/HomeController.cs
@@ -1,15 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using PlacesTracker.Models;
 
 namespace PlacesTracker.Controllers
 {
-    public class HomeController : Controller
-    {
+		public class HomeController : Controller
+		{
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+			[HttpGet("/")]
+			public ActionResult Index()
+			{
+				return View();
+			}
 
-    }
+			// [HttpGet("/places/new")]
+			// public ActionResult New()
+			// {
+			// 	return View();
+			// }
+
+			// [HttpPost("/places")]
+			// public ActionResult Create(string city)
+			// {
+			// 	Place _ = new(city);
+			// 	return RedirectToAction("Index");
+			// }
+			
+			// [HttpGet("/places/{id}")]
+			// public ActionResult Show(int id)
+			// {
+			// 	Place foundPlace = Place.Find(id);
+			// 	return View(foundPlace);
+			// }
+		}
 }
