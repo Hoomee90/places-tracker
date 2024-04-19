cdb451191ec90e82f206291b6324e3f76f9dc2cf
Samantha Callie <Hoomee90@gmail.com>
2024-04-19 14:37:39 -0700

    create views and more controller logic

--- PlacesTracker/Controllers/PlacesController.cs
+++ PlacesTracker/Controllers/PlacesController.cs
@@ -1,15 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using PlacesTracker.Models;
 
 namespace PlacesTracker.Controllers
 {
-    public class PlacesController : Controller
-    {
+	public class PlacesController : Controller
+	{
+		[HttpGet("/places")]
+		public ActionResult Index()
+		{
+			List<Place> places = Place.GetAll();
+			return View(places);
+		}
 
-      [HttpGet("/places")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+		[HttpGet("/places/new")]
+		public ActionResult New()
+		{
+			return View();
+		}
 
-    }
+		[HttpPost("/places")]
+		public ActionResult Create(string city)
+		{
+			Place _ = new(city);
+			return RedirectToAction("Index");
+		}
+		
+		[HttpGet("/places/{id}")]
+		public ActionResult Show(int id)
+		{
+			Place foundPlace = Place.Find(id);
+			return View(foundPlace);
+		}
+	}
 }
