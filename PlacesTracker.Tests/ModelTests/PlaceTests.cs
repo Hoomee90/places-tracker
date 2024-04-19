59d5ed1d6bc0e6497da59bdc3d84cb6891214ea5
Samantha Callie <Hoomee90@gmail.com>
2024-04-19 14:02:55 -0700

    add instance tracking to place class

--- PlacesTracker.Tests/ModelTests/PlaceTests.cs
+++ PlacesTracker.Tests/ModelTests/PlaceTests.cs
@@ -1,22 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PlacesTracker.Models;
+using System;
+using System.Collections.Generic;
 
 namespace PlacesTracker.Tests
 {
-  [TestClass]
-  public class PlaceTests
-  {
-    [TestMethod]
+	[TestClass]
+	public class PlaceTests : IDisposable
+	{
+		public void Dispose()
+		{
+			Place.ClearAll();
+		}
+		
+		[TestMethod]
 		public void PlaceConstructor_CreateInstanceOfPlace_Place()
 		{
-			Place newPlace = new("Winnipeg");
+			Place newPlace = new("Fargo");
 			Assert.AreEqual(typeof(Place), newPlace.GetType());
 		}
 		
 		[TestMethod]
 		public void GetCityName_ReturnsCityName_IntArray()
 		{
-			string cityName = "Fargo";
+			string cityName = "Williston";
 			Place newPlace = new(cityName);
 			string result = newPlace.CityName;
 			Assert.AreEqual(cityName, result);
@@ -25,10 +32,32 @@ namespace PlacesTracker.Tests
 		[TestMethod]
 		public void SetCityName_SetsValueOfCityName_Void()
 		{
-			Place newPlace = new("Williston");
-			string newCityName = "Minneapolis";
+			Place newPlace = new("Minneapolis");
+			string newCityName = "Winnipeg";
 			newPlace.CityName = newCityName;
 			Assert.AreEqual(newCityName, newPlace.CityName);
 		}
-  }
+		
+		[TestMethod]
+		public void GetAll_ReturnsAllPlaceInstances_List()
+		{
+			Place place1 = new("Regina");
+			Place place2 = new("Saskatoon");
+			Place place3 = new("Omaha");
+			List<Place> expected = new() { place1, place2, place3 };
+			List<Place> actualResult = Place.GetAll();
+			CollectionAssert.AreEqual(expected, actualResult);
+		}
+		
+		[TestMethod]
+		public void ClearAll_DeletesAllPlaceInList_Void()
+		{
+			_ = new Place("Bloomington");
+			_ = new Place("Woodbury");
+			_ = new Place("Hennepin");
+			List<Place> expected = new() { };
+			Place.ClearAll();
+			CollectionAssert.AreEqual(expected, Place.GetAll());
+		}
+	}
 }
--- PlacesTracker/Models/Place.cs
+++ PlacesTracker/Models/Place.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
+
 namespace PlacesTracker.Models
 {
 	public class Place
 	{
 		
 		public string CityName { get; set; }
+		private static List<Place> _instances = new() { };
+		
 		public Place(string city)
 		{
 			CityName = city;
+			_instances.Add(this);
+		}
+		
+		public static List<Place> GetAll()
+		{
+			return _instances;
+		}
+		
+		public static void ClearAll()
+		{
+			_instances.Clear();
 		}
 	}
 }
